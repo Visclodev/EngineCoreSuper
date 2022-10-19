1f3ce29fea7b02172854729a50f1dffbf9bc89dc
schneider <Yobobis@gmail.com>
2022-10-19 17:35:31 +0200

    [ADD] engine & unfinished include dir

---
+++ include/EngineCoreSuper.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** EngineSuperCore
+** File description:
+** Engine
+*/
+
+#ifndef ENGINE_HPP_
+#define ENGINE_HPP_
+
+#include "../src/Entity.hpp"
+#include "../src/SparseArray.hpp"
+#include "../src/Registry.hpp"
+#include "../src/RegistryManager.hpp"
+#include "../src/PlayerPrefs.hpp"
+
+#endif /* !ENGINE_HPP_ */
