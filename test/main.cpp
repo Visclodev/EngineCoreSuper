07c18dd14d885f454b29a52872a3298fa8d40494
schneider <Yobobis@gmail.com>
2022-10-21 17:03:55 +0200

    [ADD] added a some test files to learn/try the ecs

---
+++ test/main.cpp
@@ -0,0 +1,68 @@
+/*
+** EPITECH PROJECT, 2022
+** EngineCoreSuper
+** File description:
+** main
+*/
+
+#include "../include/EngineCoreSuper.hpp"
+
+class Player{
+    public:
+        Player(std::string name_, int health_):
+        name(name_), health(health_)
+        {}
+        ~Player()
+        {}
+        std::string name;
+        int health;
+    private:
+};
+
+void print_infos(eng::Registry &reg, eng::Entity &baba)
+{
+    std::cout << reg.getComponents<Player>()[baba.getId()].value().name << " has ";
+    std::cout << reg.getComponents<Player>()[baba.getId()].value().health;
+    std::cout << " health points!" << std::endl;
+
+    std::cout << "He is at: {" << reg.getComponents<eng::Position>()[baba.getId()].value().x;
+    std::cout << ", " << reg.getComponents<eng::Position>()[baba.getId()].value().y;
+    std::cout << "}" << std::endl;
+}
+
+void addText(eng::Registry &r)
+{
+    eng::Entity text = r.spawnEntity();
+
+    r.emplaceComponent(text, eng::Position(300, 1080 / 2, 0));
+    r.emplaceComponent(text, eng::Writable("escape_text", "Press esc. key to exit"));
+}
+
+int main(void)
+{
+    eng::Registry reg;
+    reg.setName("Registry one");
+    eng::Entity baba = reg.spawnEntity();
+    eng::GraphicSystems gfx(1920, 1080, "Coucou");
+
+    reg.registerComponents(eng::SparseArray<Player>());
+    reg.registerComponents(eng::SparseArray<eng::Position>());
+    reg.registerComponents(eng::SparseArray<eng::Drawable>());
+    reg.registerComponents(eng::SparseArray<eng::Writable>());
+    reg.emplaceComponent(baba, Player("baba", 56));
+    reg.emplaceComponent(baba, eng::Position(13, 13, 0));
+    reg.emplaceComponent(baba, eng::Drawable("../assets/logo.png"));
+    addText(reg);
+
+    print_infos(reg, baba);
+
+    while (gfx.isWindowOpen()) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            gfx.getRenderWindow().close();
+        gfx.clear();
+        gfx.drawSystem(reg);
+        gfx.writeSystem(reg);
+        gfx.display();
+    }
+    return (0);
+}
