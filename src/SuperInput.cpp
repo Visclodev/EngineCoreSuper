42b63cd45bd044b9dbe4a66bce6205debaf549f9
schneider <Yobobis@gmail.com>
2022-11-02 23:30:29 +0100

    [ADD] Progress on input interface

--- src/SuperInput.cpp
+++ src/SuperInput.cpp
@@ -14,3 +14,95 @@ eng::SuperInput::SuperInput()
 eng::SuperInput::~SuperInput()
 {
 }
+
+void eng::SuperInput::updateEvents(sf::Window &w)
+{
+    // Update every key events
+    for (auto it = _keyEvents.begin(); it != _keyEvents.begin(); it++) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(it->first))) {
+            for (auto i = it->second.begin(); i != it->second.end(); i++) {
+                _inputMap[*i][2] = _inputMap[*i][0];
+                _inputMap[*i][0] = 1.0f;
+            }
+        }
+    }
+
+    // Update every joystick button event
+
+    // Update every joystick analog event
+
+    // Update every mouse button event
+}
+
+void eng::SuperInput::addAction(std::string action, float deadzone)
+{
+    _inputMap[action] = {0.0f, deadzone, 0.0f};
+}
+
+// Add events
+
+void eng::SuperInput::addEvent(std::string action, eng::SuperInput::Key keyEvent)
+{
+    _keyEvents[(sf::Keyboard::Key)keyEvent].push_back(action);
+}
+
+void eng::SuperInput::addEvent(std::string action, eng::SuperInput::JoyAnalog joyEvent)
+{
+    _analogEvents[joyEvent].push_back(action);
+}
+
+void eng::SuperInput::addEvent(std::string action, eng::SuperInput::JoyButton joyEvent)
+{
+    _buttonEvents[joyEvent].push_back(action);
+}
+
+void eng::SuperInput::addEvent(std::string action, eng::SuperInput::MouseButton mouseEvent)
+{
+    _mouseEvents[mouseEvent].push_back(action);
+}
+
+void eng::SuperInput::clearEvent(std::string action)
+{
+    // Clear key events related to action
+    for (auto it = _keyEvents.begin(); it != _keyEvents.end(); it++)
+        for (auto i = it->second.begin(); i != it->second.end(); i++)
+            if (*i == action)
+                it->second.erase(i);
+    // Clear mouse events related to action
+    for (auto it = _mouseEvents.begin(); it != _mouseEvents.end(); it++)
+        for (auto i = it->second.begin(); i != it->second.end(); i++)
+            if (*i == action)
+                it->second.erase(i);
+    // Clear joyAnalog events related to action
+    for (auto it = _analogEvents.begin(); it != _analogEvents.end(); it++)
+        for (auto i = it->second.begin(); i != it->second.end(); i++)
+            if (*i == action)
+                it->second.erase(i);
+    // Clear joyButton events related to action
+    for (auto it = _buttonEvents.begin(); it != _buttonEvents.end(); it++)
+        for (auto i = it->second.begin(); i != it->second.end(); i++)
+            if (*i == action)
+                it->second.erase(i);
+}
+
+// Action Getters
+
+float eng::SuperInput::getActionStrength(std::string action)
+{
+    return _inputMap[action][0] - _inputMap[action][1];
+}
+
+bool eng::SuperInput::isActionPressed(std::string action)
+{
+    return (_inputMap[action][0] == 1.0f);
+}
+
+bool eng::SuperInput::isActionJustPressed(std::string action)
+{
+    return (_inputMap[action][0] == 1.0f && _inputMap[action][2] < 1.0f);
+}
+
+bool eng::SuperInput::isActionJustReleased(std::string action)
+{
+    return (_inputMap[action][0] < 1.0f && _inputMap[action][2] == 1.0f);
+}
--- src/SuperInput.hpp
+++ src/SuperInput.hpp
@@ -10,27 +10,71 @@
 
 #include <iostream>
 #include <map>
+#include <array>
+#include <SFML/Window.hpp>
 
 namespace eng
 {
-    enum InputType {
-        physical_key,
-        key,
-        joy_button,
-        joy_axis,
-        mouse_button,
-    };
 
     class SuperInput {
+        enum class JoyAnalog {
+            leftStickLeftAxis, leftStickRightAxis,
+            leftStickUpAxis, leftStickDownAxis,
+            rightStickLeftAxis, rightStickRightAxis,
+            rightStickUpAxis, rightStickDownAxis,
+            leftTrigger, rightTrigger
+        };
+
+        // a = cross, Nintendo B, Xbox A
+        // b = circle, Nintendo A, Xbox B
+        // x = square, Nintendo Y, Xbox X
+        // y = triangle, Nintendo X, Xbox Y
+        // share = Nintendo Screenshot, Xbox Share, ps5 microphone
+        enum class JoyButton {
+            a, b, x, y, up, down, right, left, start, select,
+            l1, l2, l3, r1, r2, r3, home, share
+        };
+
+        enum Key {
+            a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w,
+            x, y, z, num0, num1, num2, num3, num4, num5, num6, num7, num8, num9,
+            escape,
+            leftctrl, leftshift, leftalt, leftsystem,
+            rightctrl, rightshift, rightalt, rightsystem,
+            menu, leftbracket, rightbracket, semicolon, comma, period, quote,
+            slash, backslash, tilde, equal, hyphen, space, enter, backspace,
+            //aligned to sf::Keyboard values
+            tab, left = 71, right = 72, up = 73, down = 74
+        };
+
+        enum class MouseButton {
+            left, right, middle, wheel, wheelUp, wheelDown, forward, backward
+        };
+
         public:
             SuperInput();
             ~SuperInput();
 
-            void addAction(std::string name);
-            void addEvent(std::string action, InputType type);
+            void addAction(std::string name, float deadzone = 0.1f);
+            void addEvent(std::string action, eng::SuperInput::Key keyEvent);
+            void addEvent(std::string action, eng::SuperInput::JoyAnalog joyEvent);
+            void addEvent(std::string action, eng::SuperInput::JoyButton joyEvent);
+            void addEvent(std::string action, eng::SuperInput::MouseButton buttonEvent);
+            void clearEvent(std::string action);
+
+            void updateEvents(sf::Window &w);
+
+            float getActionStrength(std::string action);
+            bool isActionPressed(std::string action);
+            bool isActionJustPressed(std::string action);
+            bool isActionJustReleased(std::string action);
         protected:
         private:
-            std::map<std::string, float> _inputMap;
+            std::map<std::string, std::array<float, 3>> _inputMap;
+            std::map<sf::Keyboard::Key, std::vector<std::string>> _keyEvents;
+            std::map<eng::SuperInput::JoyAnalog, std::vector<std::string>> _analogEvents;
+            std::map<eng::SuperInput::JoyButton, std::vector<std::string>> _buttonEvents;
+            std::map<eng::SuperInput::MouseButton, std::vector<std::string>> _mouseEvents;
     };
 } // namespace eng
 
