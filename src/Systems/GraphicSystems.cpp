a411e71990d721a60591cf11a06a6c3890ad42d9
Vivant Gattigues <vivant.garrigues@epitech.eu>
2022-11-11 14:30:57 +0100

    [FIX] write system on graphic system

--- src/Systems/GraphicSystems.cpp
+++ src/Systems/GraphicSystems.cpp
@@ -107,15 +107,20 @@ void eng::GraphicSystems::writeSystem(Registry &r)
 {
     auto &positions = r.getComponents<Position>();
     auto &texts = r.getComponents<Writable>();
+    int x = 0;
+    int y = 0;
 
     for (int i = 0; i < texts.size() && i < positions.size(); i++) {
         if (texts[i].has_value() && positions[i].has_value()) {
             auto &text = texts[i].value();
             auto &position = positions[i].value();
-            
-            text._txt.setPosition(position.x + text._txt.getPosition().x, position.y + text._txt.getPosition().y);
+            x = text._txt.getPosition().x;
+            y = text._txt.getPosition().y;
+
+            text._txt.setPosition(position.x + x, position.y + y);
             text._txt.setRotation(position.rotation);
             _window.draw(text._txt);
+            text._txt.setPosition(x, y);
         }
     }
 }
