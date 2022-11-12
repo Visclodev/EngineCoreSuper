b3116c58be27dc5f986e7c775311bdb6875a0996
Vivant Gattigues <vivant.garrigues@epitech.eu>
2022-11-12 08:50:30 +0100

    [FIX] default value on frame rate limit

--- src/Systems/GraphicSystems.cpp
+++ src/Systems/GraphicSystems.cpp
@@ -17,7 +17,9 @@ _window(sf::VideoMode(width, height, 256), name), _clock(sf::Clock())
     //sf::Texture iconText;
     //iconText.loadFromFile("../assets/logo.png");
     //_window.setIcon(32, 32, iconText.copyToImage().getPixelsPtr());
-    _window.setFramerateLimit(60);
+    unsigned int fps = 60;
+    _window.setFramerateLimit(fps);
+    _frameRateLimit = fps;
 }
 
 eng::GraphicSystems::~GraphicSystems()
